50c00f1e1662073f2ee72725ef66134a257211dc
”SetterVet” <kosvit1408@gmail.com>
2018-04-20 21:05:55 +0300

    Part6

--- students/views.py
+++ students/views.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from django.shortcuts import render
 from django.http import HttpResponse
 
@@ -18,7 +19,7 @@ def students_delete(request, sid):
 
 # Views for Groups
 def groups_list(request):
-    return HttpResponse('<h1>Groups Listing</h1>')
+    return render(request, 'students/groups_list.html', {})
 
 
 def groups_add(request):
@@ -32,3 +33,30 @@ def groups_edit(request, gid):
 def groups_delete(request, gid):
     return HttpResponse('<h1>Delete Group %s</h1>' % gid)
 # Create your views here.
+def students_list(request):
+    students = (
+    {'id': 1,
+    'first_name': u'Віталій',
+    'last_name': u'Костирка',
+    'ticket': 235,
+    'image': 'img/bv1.jpg'},
+    {'id': 2,
+    'first_name': u'Дудаp',
+    'last_name': u'Юрій',
+    'ticket': 2123,
+    'image': 'img/bv2.jpg'},
+    {'id': 3,
+     'first_name': u'Маркелло',
+     'last_name': u'Натанаїл',
+     'ticket': 2124,
+     'image': 'img/SetterVet.jpg'}
+    )
+    return render(request, 'students/students_list.html',{'students': students})
+
+def visiting(request):
+    return render(request, 'students/visiting.html', {})
+def visiting(request):
+    month=()
+    for i in range(31):
+        month[i]=i+1
+    return render(request, 'students/visiting.html', {'month': month})
--- studentsdb/settings.py
+++ studentsdb/settings.py
@@ -10,6 +10,7 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+from django.conf import global_settings
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
@@ -82,3 +83,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+PORTAL_URL = 'http://localhost:8000'
+
+TEMPLATE_CONTEXT_PROCESSORS = global_settings.TEMPLATE_CONTEXT_PROCESSORS + ("django.core.context_processors.request","studentsdb.context_processors.students_proc", )
